46ec7d1c1a8b671e58c0a27d734f68ffceef8da3
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-04-27 17:27:53 +0200

    process traits from excel

---
+++ R/linux_diversity_patterns.R
@@ -0,0 +1,24 @@
+## "range"
+
+library(dplyr)
+library(readxl)
+library(tidyr)
+library(stringr)
+
+traits <- read_excel("~/Dropbox/Linux_project/3_traits/traits_sample.xlsx", na = "NA")
+
+distscop <- traits %>% 
+  filter(!is.na(Packages)) %>% 
+  group_by(Distribution) %>% 
+  do(data_frame(scope = str_split(.$Scope, ","))) %>% 
+  ungroup %>% 
+  unnest(scope)
+
+distscop %>% select(scope) %>% distinct
+
+distscop %>% 
+  group_by(Distribution) %>% 
+  tally %>% 
+  arrange(desc(n)) %>% 
+  .[["n"]] %>% 
+  plot(type = "l")
