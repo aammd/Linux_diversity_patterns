5fb2e8d0fe25e305bb047b34dadf2ab2192b7191
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-04-28 16:48:53 +0200

    sections and tidying

--- R/linux_diversity_patterns.R
+++ R/linux_diversity_patterns.R
@@ -1,5 +1,9 @@
 ## "range"
 
+
+# load packages and data ------------------------------
+
+
 library(dplyr)
 library(googlesheets)
 library(tidyr)
@@ -9,6 +13,10 @@ traits_sheet <- gs_title("Linux_Traits.xlsx")
 
 traits <- gs_read_csv(traits_sheet)
 
+
+# splits scopes and trims -----------------------------
+
+
 distro_scope <- traits %>% 
   filter(!is.na(Packages)) %>% 
   group_by(Distribution) %>% 
@@ -17,19 +25,7 @@ distro_scope <- traits %>%
   unnest(scope) %>% 
   mutate(scope = str_trim(scope))
 
-
-distscop %>% 
-  group_by(Distribution) %>% 
-  tally %>% 
-  rename(abd = n) %>% 
-  group_by(abd) %>% 
-  tally %>% 
-  arrange(n)
-
-
 # generalist specialist -------------------------------
-
-distro_trim <- distscop
 
 
 rare_scopes <- distro_trim %>% 
@@ -38,4 +34,11 @@ rare_scopes <- distro_trim %>%
 ## betware fucking whitespace
   
 
+distscop %>% 
+  group_by(Distribution) %>% 
+  tally %>% 
+  rename(abd = n) %>% 
+  group_by(abd) %>% 
+  tally %>% 
+  arrange(n)
 
