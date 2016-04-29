6c7de33ef23d097a830a26b1a68ddad95ba071ad
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-04-29 09:33:17 +0200

    code for a speciation area figure

--- R/linux_diversity_patterns.R
+++ R/linux_diversity_patterns.R
@@ -5,19 +5,22 @@
 
 
 library(dplyr)
-library(googlesheets)
+library(readxl)
 library(tidyr)
 library(stringr)
+library(ggplot2)
 
-traits_sheet <- gs_title("Linux_Traits.xlsx")
 
-traits <- gs_read_csv(traits_sheet)
+traits_sheet <- read_excel("~/Dropbox/Linux_project/3_traits/Linux_traits.xlsx")
 
 
 # splits scopes and trims -----------------------------
 
+traits_sheet %>% 
+  filter(is.na(Packages)) %>% 
+  View
 
-distro_scope <- traits %>% 
+distro_scope <- traits_sheet %>% 
   filter(!is.na(Packages)) %>% 
   group_by(Distribution) %>% 
   do(data_frame(scope = str_split(.$Scope, ","))) %>% 
@@ -28,11 +31,37 @@ distro_scope <- traits %>%
 # generalist specialist -------------------------------
 
 
-rare_scopes <- distro_trim %>% 
+rare_scopes <- distro_scope %>% 
   filter(!str_detect(scope, "Desktop"),
          !str_detect(scope, "Live"))
-## betware fucking whitespace
   
+
+
+# SAR -------------------------------------------------
+
+## log log?
+
+## vector of sample sizes
+## 
+scopelist <- rare_scopes %>% 
+  select(scope) %>% 
+  unique %>% 
+  .[[1]]
+
+
+pairs <- replicate(100, sample(scopelist, 2), simplify = FALSE)
+
+
+pairs %>% 
+  purrr::map_df(~ rare_scopes %>% 
+               group_by(Distribution) %>% 
+               filter(scope %in% .x),
+               .id = "sampletime"
+  ) %>% 
+  View
+             
+# plots -----------------------------------------------
+
 
 distscop %>% 
   group_by(Distribution) %>% 
@@ -40,5 +69,101 @@ distscop %>%
   rename(abd = n) %>% 
   group_by(abd) %>% 
   tally %>% 
-  arrange(n)
+  arrange(n) %>% 
+  ggplot(aes(x = abd, y = n)) + 
+  geom_point() +
+  geom_line() +
+  xlab("number of Scopes") +
+  ylab("number of Distribution")
+  
 
+distro_scope %>% 
+  select(scope) %>% 
+  distinct
+
+distscop %>% 
+  group_by(Distribution) %>% 
+  tally %>% 
+  rename(abd = n) %>% 
+  arrange(desc(abd)) %>% 
+  View
+  
+
+
+dist_wide <- distro_scope %>% 
+  mutate(abd = 1) %>% 
+  spread(scope, abd, fill = 0)
+distmat <- as.matrix(dist_wide[-1])
+rownames(distmat) <- dist_wide$Distribution
+
+library(metacom)
+Imagine(as.matrix(dist_wide[-1]), fill = FALSE)
+library(bipartite)
+plotweb(distmat)
+
+
+# ok maybe we consider countries ----------------------
+
+traits_nonglobal <- traits_sheet %>% 
+  filter(Country != "Global")
+  
+
+
+traits_nonglobal %>% 
+  group_by(Country) %>% 
+  tally %>% 
+  arrange(desc(n)) %>% 
+  ggplot(aes(x = seq_along(n), y = n)) + geom_point()
+
+
+dist_wide <- traits_nonglobal %>% 
+  select(Distribution, Country) %>% 
+  mutate(abd = 1) %>% 
+  spread(Country, abd, fill = 0)
+distmat <- as.matrix(dist_wide[-1])
+rownames(distmat) <- dist_wide$Distribution
+
+library(metacom)
+Imagine(as.matrix(dist_wide[-1]), fill = FALSE)
+library(bipartite)
+plotweb(distmat)
+
+
+# fix country names -----------------------------------
+
+library(countrycode)
+library(geonames)
+
+
+country_traits_code <- traits_nonglobal %>% 
+  select(Country) %>% 
+  distinct %>% 
+  mutate(cc = countrycode(Country, "country.name", "iso2c"))
+
+geonames::GNcountryInfo("CN")
+
+country_size <- country_traits_code %>% 
+  group_by(Country, cc) %>% 
+  do(GNcountryInfo(.$cc))
+
+sizes <- country_size %>% 
+  select(Country, cc, areaInSqKm, population)
+
+traits_nonglobal %>%
+  group_by(Country) %>% 
+  tally %>% 
+  left_join(sizes) %>% 
+  mutate(area = as.numeric(population)/1000000) %>% 
+  arrange(area) %>% 
+  mutate(cum_n = n,
+         cum_a = area) %>% 
+  ggplot(aes(x = cum_a, y = cum_n)) + 
+  scale_y_log10() +
+  scale_x_log10() +
+  theme_minimal() +
+  stat_smooth(method = "glm",  method.args = list(family = "poisson"), colour = "darkgrey") + 
+  geom_point() +
+  xlab("Population (millions)") +
+  ylab("Number of distros")
+
+ggsave("figures/speciation_area.png", width = 5, height = 3.5)
