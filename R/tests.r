dad485393eea3a3890eedbe0760808120732fefd
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-04-29 10:56:44 +0200

    tweaking script to use the latest excel data

--- R/tests.r
+++ R/tests.r
@@ -1,14 +1,18 @@
 
+library(readxl)
+traits_sheet <- read_excel("data/Linux_traits.xlsx",
+                           col_types = c("text", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "text", "text", "text", "text", "numeric"), na = c("NA"))
 
-dat <- read.csv("Linux_traits.csv")
+dat <- traits_sheet
+
 dat$Scope <- as.character(dat$Scope)
 head(dat)
 
 # remove NAs
 dat <- dat[is.na(dat$Packages) == FALSE,]
 
-pdf(file="niche_breath.pdf", width=10, height=3.5)
-par(mfrow=c(1,3))
+pdf(file = "figures/niche_breath.pdf", width=10, height=3.5)
+par(mfrow = c(1,3))
 
 # ---------------------------------------------------------------------
 ##### HPD vs # of PACKAGES #########
@@ -16,11 +20,11 @@ plot(log10(HPD)~log10(Packages), data=dat, frame=FALSE,
      xlab="log_10 # packages", ylab="log_10 HPD")
 mtext("A", adj=-0.14, font=2)
 
-logHPD <- log10(dat$HPD)
-logPackages <- log10(dat$Packages)
+dat$logHPD <- log10(dat$HPD)
+dat$logPackages <- log10(dat$Packages)
 
 #m1 <- loess(logHPD~logPackages)
-m1 <- lm(logHPD~logPackages)
+m1 <- lm(logHPD~logPackages, data = dat)
 newdata <- data.frame(logPackages =seq(1, 5, by=0.1))
 preds <- predict(m1, newdata=newdata, se.fit=TRUE )
 lines(newdata$logPackages, preds$fit, col="red", lwd=2)
@@ -41,21 +45,21 @@ plot(log10(HPD) ~ jitter(Scope_width, factor=0.5), data=dat, frame=FALSE,
      xlab="# applications", ylab="log_10 HPD")
 mtext("B", adj=-0.14, font=2)
 
-ScopeWidth <- dat$Scope_width
+hpd_scope <- dat[names(dat) %in% c("logHPD", "Scope_width")]
 
-m0 <- lm(logHPD ~ 1)
-m1 <- lm(logHPD~ScopeWidth)
-m2 <- lm(logHPD~poly(ScopeWidth, 2), data=na.omit(dat))
-m3 <- lm(logHPD~poly(ScopeWidth, 3), data=na.omit(dat))
+m0 <- lm(logHPD ~ 1, data = hpd_scope)
+m1 <- lm(logHPD~Scope_width, data = hpd_scope)
+m2 <- lm(logHPD~poly(Scope_width, 2), data=na.omit(hpd_scope))
+m3 <- lm(logHPD~poly(Scope_width, 3), data=na.omit(hpd_scope))
 
 AIC(m0, m1, m2, m3)
 
-newdata <- data.frame(ScopeWidth =seq(1, 5, by=0.1))
+newdata <- data.frame(Scope_width =seq(1, 5, by=0.1))
 preds <- predict(m1, newdata=newdata, se.fit=TRUE )
-lines(newdata$ScopeWidth, preds$fit, col="red", lwd=2)
-lines(newdata$ScopeWidth, preds$fit + preds$se.fit , 
+lines(newdata$Scope_width, preds$fit, col="red", lwd=2)
+lines(newdata$Scope_width, preds$fit + preds$se.fit , 
       col="red", lwd=1, lty=2)
-lines(newdata$ScopeWidth, preds$fit - preds$se.fit , 
+lines(newdata$Scope_width, preds$fit - preds$se.fit , 
       col="red", lwd=1, lty=2)
 
 # ---------------------------------------------------------------
@@ -64,29 +68,33 @@ plot(log10(HPD) ~ jitter(Scope_special, factor=0.5), data=dat, frame=FALSE,
      xlab="# specialized applications", ylab="log_10 HPD")
 mtext("C", adj=-0.14, font=2)
 
-ScopeSpec <- dat$Scope_width
+# ScopeSpec <- dat$Scope_width
 
-m0 <- lm(logHPD ~ 1)
-m1 <- lm(logHPD~ScopeSpec)
-m2 <- lm(logHPD~poly(ScopeSpec, 2), data=na.omit(dat))
-m3 <- lm(logHPD~poly(ScopeSpec, 3), data=na.omit(dat))
+hpd_spec <- dat[names(dat) %in% c("logHPD", "Scope_special")]
+
+m0 <- lm(logHPD ~ 1, data = hpd_spec)
+m1 <- lm(logHPD~Scope_special, data = hpd_spec)
+m2 <- lm(logHPD~poly(Scope_special, 2), data=na.omit(hpd_spec))
+m3 <- lm(logHPD~poly(Scope_special, 3), data=na.omit(hpd_spec))
 
 AIC(m0, m1, m2, m3)
 
-newdata <- data.frame(ScopeSpec =seq(0, 5, by=0.1))
+newdata <- data.frame(Scope_special =seq(0, 5, by=0.1))
 preds <- predict(m1, newdata=newdata, se.fit=TRUE )
-lines(newdata$ScopeSpec, preds$fit, col="red", lwd=2)
-lines(newdata$ScopeSpec, preds$fit + preds$se.fit , 
+lines(newdata$Scope_special, preds$fit, col="red", lwd=2)
+lines(newdata$Scope_special, preds$fit + preds$se.fit , 
       col="red", lwd=1, lty=2)
-lines(newdata$ScopeSpec, preds$fit - preds$se.fit , 
+lines(newdata$Scope_special, preds$fit - preds$se.fit , 
       col="red", lwd=1, lty=2)
 
-summary(lm(log10(HPD)~Scope_width, data=dat))
 dev.off()
+
+summary(lm(log10(HPD)~Scope_width, data=dat))
+
 
 # ---------------------------------------------------------------
 
-pdf(file="parents.pdf", width=43, height=5)
+pdf(file="figures/parents.pdf", width=43, height=5)
 boxplot(log10(HPD) ~ Parent, data=dat, 
         ylab="log_10 HPD", col="grey")
 dev.off()
